c292bd8a3fd0ff180375b0dab77ac169d4c9b53e
dansubaie2 <dansubaie029@gmail.com>
2026-09-12 11:26:38 +0300

    Add target users, FAQ and supportef files sections

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import { Hero } from './features/landing/components/hero';
 import HowItWorks from './features/landing/components/HowItWorks';
 import Features from './features/landing/components/Features';
+import TargetUsers from './features/landing/components/TargetUsers';
 import { Footer } from './features/landing/components/Footer';
 import './App.css';
 import { Header } from './features/landing/components/Header';
+import { FAQ } from './features/landing/components/FAQ';
+import { SupportedFiles } from './features/landing/components/SupportedFiles';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
       <Hero />
       <HowItWorks />
       <Features />
+      <TargetUsers />
+      <SupportedFiles />
+      <FAQ />
       <Footer />
 
     </>
