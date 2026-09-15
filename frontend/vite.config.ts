57f618c802aae924301188ff1f75379a3632f2de
dansubaie2 <dansubaie029@gmail.com>
2026-09-15 15:38:01 +0300

    remove invoice detail changes from readme branch

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,10 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'https://invoi.jxtechstudio.com',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+    open: true
+  }
 })
